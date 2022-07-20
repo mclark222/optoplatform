b60fecf0e5dd73a9e5a6d59b1921ea16a7fa104f
optofinancial <optofinancial@gmail.com>
2022-07-20 06:55:42 +0000

    V1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def homepage
+    render({ :template => "homepage.html.erb" })
+  end
+
+  def budgeting_and_planning
+    render({ :template => "homepage.html.erb" })
+  end 
+
+  def student_loans
+    render({ :template => "homepage.html.erb" })
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
-
-
+  get("/home", { :controller => "application", :action => "homepage" })
+  #get("/budgeting-and-planning", { :controller => "application", :action => "budgeting_and_planning" })
+  #get("/student-loans", { :controller => "application", :action => "student_loans" })
 end
