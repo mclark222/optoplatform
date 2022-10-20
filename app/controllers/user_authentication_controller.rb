17dcba5d9b609b6b9450affbe631b291b7771de4
MatthewChang908 <matthew.chang.908@gmail.com>
2022-10-20 17:54:37 +0000

    go back button

--- app/controllers/user_authentication_controller.rb
+++ app/controllers/user_authentication_controller.rb
@@ -1,6 +1,7 @@
 class UserAuthenticationController < ApplicationController
   # Uncomment line 3 in this file and line 5 in ApplicationController if you want to force users to sign in before any other actions.
-  skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie] })
+  skip_before_action(:force_user_sign_in, { :only => [:sign_up_form, :create, :sign_in_form, :create_cookie, :forgot_password] })
+
 
   def sign_in_form
     render({ :template => "user_authentication/sign_in.html.erb" })
@@ -39,9 +40,9 @@ class UserAuthenticationController < ApplicationController
   end
 
   def forgot_password
-    render({ :template => "user_mailer/password_reset.html.erb" )
+    render({ :template => "user_mailer/password_reset.html.erb" })
   end
-
+  
   def create
     @user = User.new
     @user.email = params.fetch("query_email")
--- config/routes.rb
+++ config/routes.rb
@@ -335,14 +335,14 @@ Rails.application.routes.draw do
   # Routes for the User account:
 
   # SIGN UP FORM
-  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" }) 
-
-  # FORGOT PASSWORD
-  get("/user_forgot_password", { :controller => "user_authentication", :action => "forgot_password" })     
+  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })   
 
   # CREATE RECORD
   post("/insert_user", { :controller => "user_authentication", :action => "create"  })
-      
+  
+  # FORGOT PASSWORD
+  get("/user_forgot_password", { :controller => "user_authentication", :action => "forgot_password" }) 
+    
   # EDIT PROFILE FORM        
   get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
   # UPDATE RECORD
@@ -357,7 +357,7 @@ Rails.application.routes.draw do
   get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
   # AUTHENTICATE AND STORE COOKIE
   post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
-  
+
   # SIGN OUT        
   get("/user_sign_out", { :controller => "user_authentication", :action => "destroy_cookies" })
 
