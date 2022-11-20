c93c192cb73558c4c0fd0f23e97e4bc46dd6ee34
mclark222 <mclark22@chicagobooth.edu>
2022-11-20 20:42:52 +0000

    Plaid Development Key

--- app/domain/plaid_client.rb
+++ app/domain/plaid_client.rb
@@ -1,10 +1,10 @@
 class PlaidClient
 	def initialize
 		configuration = Plaid::Configuration.new
-		configuration.server_index = Plaid::Configuration::Environment["sandbox"]
+		configuration.server_index = Plaid::Configuration::Environment["development"]
 		# clientID and secrets to be moved to env variables
 		configuration.api_key["PLAID-CLIENT-ID"] = "62e9fc1a50f8030013968af0"
-    configuration.api_key["PLAID-SECRET"] = "e863c9a11046dec35f60ef08d7b4af"
+    configuration.api_key["PLAID-SECRET"] = "20ba4d1adc601be89d9be88472a96d"
 
 		api_client = Plaid::ApiClient.new(
 			configuration
